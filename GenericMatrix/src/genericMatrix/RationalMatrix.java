bd3d3e4732084a16fbffbf6a073e10c73d170469
muyu-chengfeng <835783185@qq.com>
2019-09-15 20:21:54 +0800

    hello

---
+++ GenericMatrix/src/genericMatrix/RationalMatrix.java
@@ -0,0 +1,27 @@
+package genericMatrix;
+
+public class RationalMatrix extends GenericMatrix<Rational> {
+
+@Override
+protected Rational add(Rational o1, Rational o2) {
+
+// TODO Auto-generated method stub
+return o1.add(o2);
+}
+
+@Override
+protected Rational multiply(Rational o1, Rational o2) {
+
+// TODO Auto-generated method stub
+return o1.multiple(o2);
+}
+
+@Override
+protected Rational zero() {
+
+// TODO Auto-generated method stub
+return new Rational(0, 1);
+}
+
+}
+
---
+++ GenericMatrix/src/genericMatrix/TestRationalMatrix.java
@@ -0,0 +1,30 @@
+package genericMatrix;
+
+
+public class TestRationalMatrix {
+
+		/**
+		* @param args
+		*/
+		public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		Rational[][] m1 = new Rational[3][3];
+		Rational[][] m2 = new Rational[3][3];
+		for (int i = 0; i < m1.length; i++) {
+		for (int j = 0; j < m1[0].length; j++) {
+		m1[i][j] = new Rational(i + 1, j + 5);
+		m2[i][j] = new Rational(i + 1, j + 6);
+		}
+		}
+		RationalMatrix rationalMatrix = new RationalMatrix();
+		System.out.println("\nm1 + m2 is ");
+		RationalMatrix.printResult(m1, m2, rationalMatrix.addMatrix(m1, m2),
+		'+');
+
+		System.out.println("\nm1 * m2 is ");
+		RationalMatrix.printResult(m1, m2,
+		rationalMatrix.multiplyMatrix(m1, m2), '*');
+		}
+
+}
